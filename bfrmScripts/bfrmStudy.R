6d4364975b9ad449c41d0f1f4105f542c14a8b7e
erichhuang <erich.huang@sagebase.org>
2012-09-15 05:37:13 +0000

    Refactored to take advantage of new features in the 'rGithubClient' package.

--- bfrmScripts/bfrmStudy.R
+++ bfrmScripts/bfrmStudy.R
@@ -12,11 +12,16 @@ require(doMC)
 require(rGithubClient)
 
 ## Source in the 'generateFacs' function from Github
-githubRepo <- 
-  downloadRepo('Sage-Bionetworks/Cancer-Pathway-Sparse-Factor-Models')
+# githubRepo <- 
+#   downloadRepo('Sage-Bionetworks/Cancer-Pathway-Sparse-Factor-Models')
+# 
+# source(paste(githubRepo@localPath, 
+#              '/factorGeneration/generateFacs.R', sep = ''))
 
-source(paste(githubRepo@localPath, 
-             '/factorGeneration/generateFacs.R', sep = ''))
+genFacsBlob <- 
+  downloadRepoBlob('Sage-Bionetworks/Cancer-Pathway-Sparse-Factor-Models',
+                   'factorGeneration/generateFacs.R')
+sourceBlob(genFacsBlob, 'factorGeneration/generateFacs.R')
 
 ## Pull in the IDs of all normalized studies
 normalizedEsets <- 
